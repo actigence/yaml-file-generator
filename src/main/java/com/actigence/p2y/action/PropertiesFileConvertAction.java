19f83fb48822473c512eba62cc1eac4b71ef8109
Anubhav <shukla.anubhav@gmail.com>
2018-08-26 00:36:41 +0530

    Integrated with properties-to-yaml-converter library

--- src/main/java/com/actigence/p2y/action/PropertiesFileConvertAction.java
+++ src/main/java/com/actigence/p2y/action/PropertiesFileConvertAction.java
@@ -1,6 +1,7 @@
 package com.actigence.p2y.action;
 
 import com.actigence.p2y.constants.PluginConstants;
+import com.anubhavshukla.p2y.converter.PropertiesToYamlConverter;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -11,87 +12,88 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.annotations.NotNull;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import org.jetbrains.annotations.NotNull;
 
 public class PropertiesFileConvertAction extends AnAction {
 
-    private static final String ACTION_TEXT = "Generate Yaml file";
+  private static final String ACTION_TEXT = "Generate Yaml file";
 
-    public PropertiesFileConvertAction() {
-        super(ACTION_TEXT);
+  private PropertiesToYamlConverter propertiesToYamlConverter = new PropertiesToYamlConverter();
+
+  public PropertiesFileConvertAction() {
+    super(ACTION_TEXT);
+  }
+
+  public void actionPerformed(AnActionEvent event) {
+    Project project = event.getProject();
+
+    Editor editor = event.getData(CommonDataKeys.EDITOR);
+    if (editor == null) {
+      return;
     }
 
-    public void actionPerformed(AnActionEvent event) {
-        Project project = event.getProject();
+    String str = getYamlString(event);
 
-        Editor editor = event.getData(CommonDataKeys.EDITOR);
-        if (editor == null) {
-            return;
-        }
+    String yamlFilePath = createYamlFile((EditorImpl) editor, str);
 
-        String str = getYamlString(event);
+    try {
+      openYamlFile(project, yamlFilePath);
+    } catch (Exception e) {
+      Messages.showMessageDialog(project, "Unable to create Yaml file.", "Error",
+          Messages.getInformationIcon());
+    }
+  }
 
-        String yamlFilePath = createYamlFile((EditorImpl) editor, str);
+  private void openYamlFile(Project project, String yamlFilePath) {
+    VirtualFile yamlVirtualFile = LocalFileSystem.getInstance()
+        .refreshAndFindFileByIoFile(new File(yamlFilePath));
+    if (project != null && yamlVirtualFile != null) {
+      new OpenFileDescriptor(project, yamlVirtualFile).navigate(true);
+    }
+  }
 
-        try {
-            openYamlFile(project, yamlFilePath);
-        } catch (Exception e) {
-            Messages.showMessageDialog(project, "Unable to create Yaml file.", "Error", Messages.getInformationIcon());
-        }
+  @NotNull
+  private String createYamlFile(EditorImpl editor, String str) {
+    VirtualFile virtualFile = editor.getVirtualFile();
+    String yamlFilePath = virtualFile.getPath().substring(0,
+        virtualFile.getPath().length() - PluginConstants.PROPERTIES_FILE_EXTENSION.length())
+        + "yml";
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(yamlFilePath))) {
+      writer.write(str);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return yamlFilePath;
+  }
+
+  @NotNull
+  private String getYamlString(AnActionEvent event) {
+    Editor editor = event.getData(CommonDataKeys.EDITOR);
+    if (editor == null) {
+      throw new RuntimeException("No editor object.");
     }
 
-    private void openYamlFile(Project project, String yamlFilePath) {
-        VirtualFile yamlVirtualFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(new File(yamlFilePath));
-        if (project != null && yamlVirtualFile != null) {
-            new OpenFileDescriptor(project, yamlVirtualFile).navigate(true);
-        }
-    }
+    String propertiesFilePath = ((EditorImpl) editor).getVirtualFile().getPath();
 
-    @NotNull
-    private String createYamlFile(EditorImpl editor, String str) {
-        VirtualFile virtualFile = editor.getVirtualFile();
-        String yamlFilePath = virtualFile.getPath().substring(0, virtualFile.getPath().length() - PluginConstants.PROPERTIES_FILE_EXTENSION.length()) + "yml";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(yamlFilePath))) {
-            writer.write(str);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return yamlFilePath;
-    }
+    //call properties to yaml converter to get yaml String and return that.
+    return propertiesToYamlConverter.toYamlString(propertiesFilePath);
+  }
 
-    @NotNull
-    private String getYamlString(AnActionEvent event) {
-        Editor editor = event.getData(CommonDataKeys.EDITOR);
-        if (editor == null) {
-            throw new RuntimeException("No editor object.");
-        }
+  @Override
+  public void update(AnActionEvent e) {
+    Editor editor = e.getData(CommonDataKeys.EDITOR);
+    e.getPresentation().setVisible(isVisible(editor));
+  }
 
-        String propertiesFilePath = ((EditorImpl) editor).getVirtualFile().getPath();
-
-        //call properties to yaml converter to get yaml String and return that.
-        //.....
-
-        return propertiesFilePath;
-    }
-
-    @Override
-    public void update(AnActionEvent e) {
-        Editor editor = e.getData(CommonDataKeys.EDITOR);
-        e.getPresentation().setVisible(isVisible(editor));
-    }
-
-    /**
-     * Show only if current file is a Properties file.
-     *
-     * @param editor
-     * @return
-     */
-    private boolean isVisible(Editor editor) {
-        return editor != null && PluginConstants.PROPERTIES_FILE_TYPE.equals(((EditorImpl) editor).getVirtualFile().getFileType());
-    }
+  /**
+   * Show only if current file is a Properties file.
+   */
+  private boolean isVisible(Editor editor) {
+    return editor != null && PluginConstants.PROPERTIES_FILE_TYPE
+        .equals(((EditorImpl) editor).getVirtualFile().getFileType());
+  }
 }
